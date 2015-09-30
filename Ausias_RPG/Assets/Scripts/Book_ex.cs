41d364d1dcc17b9d6651e5c94e339bba13132a05
cumus <ec4who@hotmail.com>
2015-09-30 07:27:32 +0200

    alpha done

--- Ausias_RPG/Assets/Scripts/Book_ex.cs
+++ Ausias_RPG/Assets/Scripts/Book_ex.cs
@@ -1,41 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Inventory_Collectable : MonoBehaviour
-{
-
+public class Door_ex : MonoBehaviour {
+	
 	public Transform other;
 	public GameObject message;
-	public InventoryItems id = InventoryItems.CAMARA;
+	public GameObject camara;
 	public int closeDistance = 2;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		// find character in scene
-
+		
 		// find scene manager in scene
-
-	
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-
+		
 		// check if character is close enough
 		if (other) {
 			Vector3 offset = other.position - transform.position;
 			if (offset.sqrMagnitude < closeDistance * closeDistance)
 			{
 				//Debug.Log("You are close");
-
 				message.SetActive(true);
-
+				
 				if (Input.GetKeyUp (KeyCode.F))
 				{
-					Collect ();
+					Application.LoadLevel(4);
 				}
+				
 			}
 			else
 			{
@@ -43,34 +42,6 @@ public class Inventory_Collectable : MonoBehaviour
 			}
 			
 		}
-
+		
 	}
-
-	void OnCollisionEnter(Collision collision)
-	{
-		if(collision.rigidbody == other)
-		{
-			message.SetActive(true);
-
-			if (Input.GetKeyUp (KeyCode.F))
-			{
-				Collect ();
-			}
-		}
-	}
-
-	void OnCollisionExit(Collision collisionInfo)
-	{
-		message.SetActive(false);
-	}
-
-	public InventoryItems Collect()
-	{
-		message.SetActive(false);
-		gameObject.SetActive (false);
-		return id;
-	}
-
-
-
 }
