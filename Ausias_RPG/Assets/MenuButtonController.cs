c73696deee79adca8c1215e80707cab515a803ed
dibu13 <dibu.13@gmail.com>
2015-09-30 02:31:26 +0200

    Main Title

---
+++ Ausias_RPG/Assets/MenuButtonController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonController : MonoBehaviour
+{
+
+    public GameObject Arrows;
+
+    void OnMouseOver()
+    {
+        Arrows.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        Arrows.SetActive(false);
+    }
+}
