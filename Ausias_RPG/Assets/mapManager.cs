85a36b7bac4c7362a6e7ccbbcbbcebfa9f449629
jordimc31 <jordiupc87@gmail.com>
2015-10-04 22:16:58 +0200

    GUI v2

---
+++ Ausias_RPG/Assets/mapManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mapManager : MonoBehaviour {
+
+	public bool active = true;
+	// Use this for initialization
+	void Start () {
+
+		if (active)
+			active = false;
+		else
+			active = true;
+		this.gameObject.SetActive (active);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowNotShow(){
+
+		if (active)
+			active = false;
+		else
+			active = true;
+		this.gameObject.SetActive (active);
+
+	}
+}
