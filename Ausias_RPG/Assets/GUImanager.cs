5654698b91af1d65ef8489ba64846c715cc43f43
jordimc31 <jordiupc87@gmail.com>
2015-10-04 22:20:54 +0200

    GUI v3

--- Ausias_RPG/Assets/GUImanager.cs
+++ Ausias_RPG/Assets/GUImanager.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class GUImanager : MonoBehaviour {
 
+	public bool active = true;
 	// Use this for initialization
 	void Start () {
-	
+
+		if (active)
+			active = false;
+		else
+			active = true;
+		this.gameObject.SetActive (active);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void ShowNotShow(){
+
+		if (active)
+			active = false;
+		else
+			active = true;
+		this.gameObject.SetActive (active);
+
 	}
 }
