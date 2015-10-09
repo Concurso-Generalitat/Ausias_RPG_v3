73336866e38c4b880316c30023de50e9443bd092
jordimc31 <jordiupc87@gmail.com>
2015-10-09 23:01:23 +0200

    intro

---
+++ Ausias_RPG/Assets/introCam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class introCam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Camera.main.fieldOfView > 40)
+		Camera.main.fieldOfView = Camera.main.fieldOfView- 0.01f;
+
+	}
+}
